a19d4181fd63639581a17b45d05bc18c4a107fb5
John Deverall <johndeverall@gmail.com>
2015-12-07 11:05:38 +0100

    Clearer project structure

---
+++ JUnitTestRulesInSerenity/TestRuleFailureCase/src/test/java/net/serenitybdd/demos/acceptance/CalculateResults.java
@@ -0,0 +1,60 @@
+package net.serenitybdd.demos.acceptance;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import net.serenitybdd.demos.calculator.CalculatorApp;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/calculator")
+@SpringApplicationConfiguration(CalculatorApp.class)
+public class CalculateResults {
+	
+	@Rule
+	public final SpringMethodRule springMethodRule = new SpringMethodRule();
+	
+	@ClassRule
+	public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+	
+	private static int count = 0;
+	
+	@BeforeClass
+	public static void beforeClass() { 
+		count++;
+	}
+	
+	@Before
+	public void thisShouldRunBeforeEachMethod() { 
+		count++;
+	}
+	
+	@Test
+	public void instantiates() { 
+		assertTrue(true);
+	}
+	
+	@Test
+	public void secondTest() { 
+		assertTrue(true);
+	}
+	
+	@AfterClass()
+	public static void test() { 
+		// Should be 3. One for the @BeforeClass and twice for the @Before run before the two unit tests.
+		assertEquals(3, count); 
+	}
+	
+}
